67db0bfc4986da12b984cd828c8887ca1781ba1a
gEvent <gevent@CSSI2015-17.local>
2015-07-27 22:20:16 -0400

    Youtube File

---
+++ youtubeStuff.py
@@ -0,0 +1,13 @@
+# embeds a video in the webpage
+        self.response.write('''<iframe id=vid1 width="300" height="200" src=https://www.youtube.com/embed/6744Ym_5Ddg frameborder="0"></iframe>
+            <iframe id=vid1 width="300" height="200" src=https://www.youtube.com/embed/ulsA8aFOkRs frameborder="0"></iframe>''')
+        self.response.write('''<iframe id=hilClint width="300" height="200" src=https://www.youtube.com/embed/6744Ym_5Ddg></iframe>
+            <iframe id=benCarso width="300" height="200" src=https://www.youtube.com/embed/ulsA8aFOkRs></iframe>''')
+# <iframe id=nameOfCand width="300" height="200" src=https://www.youtube.com/embed/IDofVid></iframe>
+
+# IDs of videos of speeches where candidates are talking about issues they support
+# jeb bush education jTWl3YoOXAc
+# ben carson UN HHvFh6lSJqk
+# chris cristie green energy uPxDnb2-aVI
+# bobby jindal education nDCU-VlSgX0
+# marco rubio military d9FVjcuz-pA
